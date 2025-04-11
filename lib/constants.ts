f0fce5711f0e15e3490f9342a14bc2343b7f60d4
Lev Natekin <lnatekin@asu.edu>
2025-04-10 17:49:59 -0700

    rnr added

---
+++ lib/constants.ts
@@ -0,0 +1,18 @@
+export const NAV_THEME = {
+  light: {
+    background: "hsl(40 66.27% 96.2%)", // from --background
+    border: "hsl(40 80% 91%)", // from --border
+    card: "hsl(40 100% 95.7%)", // from --card
+    notification: "hsl(7 82% 38%)", // from --destructive
+    primary: "hsl(204.43 100% 34%)", // from --primary
+    text: "hsl(204.43 100% 19%)", // from --foreground
+  },
+  dark: {
+    background: "hsl(40 90% 96%)", // from --background
+    border: "hsl(40 80% 91%)", // from --border
+    card: "hsl(40 90% 94%)", // from --card
+    notification: "hsl(7 82% 38%)", // from --destructive
+    primary: "hsl(187 100% 34%)", // from --primary
+    text: "hsl(187 100% 19%)", // from --foreground
+  },
+};
